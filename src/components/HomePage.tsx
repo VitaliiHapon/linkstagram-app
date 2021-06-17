b8cd924973b169582c9bfaa592c9830abf22a41e
vitalik1240 <vitalik1240@gmail.com>
2021-06-17 10:38:50 +0300

    some improvements

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserView, isMobile, MobileView } from "react-device-detect";
 import { api } from "../adapters/post";
 import PostComponent from "./PostComponent";
 import PostModal from "./PostModal";
+import '../css/home.css'
+import EditModal from "./EditModal";
 
 type HomePageState = {
     posts: api.PostInfo[]
     postMoadlVisible: boolean,
-    selectedPost:  api.PostInfo | null,
-    comments: api.PostComment[] | null
+    selectedPost: api.PostInfo | null,
+    comments: api.PostComment[] | null,
+    likeInfo: api.LikeInfo;
+    onLike: () => void;
+    onComment: () => void;
+
 }
 
 class HomePage extends React.Component<any, HomePageState>{
@@ -17,29 +24,69 @@ class HomePage extends React.Component<any, HomePageState>{
             posts: [],
             postMoadlVisible: false,
             selectedPost: null,
-            comments: []
+            comments: [],
+            onLike: () => { },
+            onComment: () => { },
+            likeInfo: {
+                isLiked: false,
+                likesCount: 0
+            },
+
         }
 
         api.getAllPosts((x) => this.setState({ posts: x }));
     }
+
+    renderPostModal() {
+        if (!this.state.postMoadlVisible || isMobile)
+            return null;
+
+        return (<PostModal onClose={() => this.setState({ postMoadlVisible: false })}
+            visible={this.state.postMoadlVisible}
+            post={this.state.selectedPost}
+            comments={this.state.comments}
+            onLike={this.state.onLike}
+            likeInfo={this.state.likeInfo}
+            onComment={this.state.onComment} />)
+    }
+
+
+    renderPost(post: any) {
+        return (
+            <div className="home-page-post">
+                <PostComponent onPostOpen={(onComment: () => void, onLike: () => void, likeInfo: api.LikeInfo, post: any, comments: any) => {
+                    this.setState({
+                        onComment: onComment,
+                        onLike: onLike,
+                        likeInfo: likeInfo,
+                        selectedPost: post,
+                        comments: comments,
+                        postMoadlVisible: true
+                    })
+                }} key={post.id} post={post} />
+            </div>
+        )
+    }
+
     render() {
-        console.log("HOME");
-        console.log(this.state.posts);
+
         return (
             <div>
-               
-                <PostModal onClose={() => this.setState({ postMoadlVisible: false })} visible={this.state.postMoadlVisible} post={this.state.selectedPost}></PostModal>
-                <div className="home-page-container">
-                    <div>
-                        {this.state.posts.map((x) => <PostComponent onPostOpen={(post: any, comments: any) => {
-                            this.setState({
-                                selectedPost:post,
-                                comments:comments,
-                                postMoadlVisible: true
-                            })
-                        }} key={x.id} post={x} />)}
+                {this.renderPostModal()}
+                <MobileView>
+                    <div className="home-page-container-mobile">
+                        <div>
+                            {this.state.posts.map((x) => this.renderPost(x))}
+                        </div>
                     </div>
-                </div>
+                </MobileView>
+                <BrowserView>
+                <div className="home-page-container-mobile">
+                        <div>
+                            {this.state.posts.map((x) => this.renderPost(x))}
+                        </div>
+                    </div>
+                </BrowserView>
             </div>
         )
     }
